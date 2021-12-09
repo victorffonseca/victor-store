8e8710dcf4897abd8e4310d0d7aa9767e4142550
Victor Fonseca <victorf.fonseca01@gmail.com>
2021-12-09 15:24:00 -0500

    Implementación de ciclo de vida de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './components/product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'victor-store';
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'Camiseta producto'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'Hoodie producto'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'Mug producto'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'Pin producto'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Sticker #1',
+      price: 80000,
+      description: 'Sticker 1 producto'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Sticker #2',
+      price: 80000,
+      description: 'Stickers 2 producto'
+    },
+  ];
+
+  clickProduct(id: number) {
+    console.log('product');
+    console.log(id);
+  }
 }
